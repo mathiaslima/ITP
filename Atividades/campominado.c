c80d8de10ccb1baa98d6979a431ed101613347bd
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-10-24 20:46:43 -0300

    Adicionada exibição das coordenadas nos cantos do mapa

--- Atividades/campominado.c
+++ Atividades/campominado.c
@@ -2,24 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-int campo[12][12] = {{0}}; // Localização das bombas
-char exibicao[10][10]; // O que vai ser exibido para o usuário
+int campo[10][10] = {{0}}; // Localização das bombas
+char exibicao[11][11]; // O que vai ser exibido para o usuário
+
 int checa_bomba (int, int);
+void prepara_campo (void);
+void prepara_exibicao (void);
 
 int
 main (void)
 {
-	int i, j, jog, lin, col;
 	srand(time(NULL));
-	// Preenche o campo aleatoriamente com 0 e 1 e preenche com '*' o que será exibido para o usuário
-	for (i = 1; i <= 10; ++i)
-	{
-		for (j = 1; j <= 10; ++j)
-		{
-			campo[i][j] = rand() % 2;
-			exibicao[i][j] = '*';
-		}
-	}
+
+    prepara_campo();
+    prepara_exibicao();
 
 	// Executa o jogo
 	do
@@ -27,17 +23,18 @@ main (void)
 		system("clear");
 
 		// Imprime o campo para o usuário
-		for (i = 1; i <= 10; ++i)
+        int i, j;
+		for (i = 0; i < 11; ++i)
 		{
-			for (j = 1; j <= 10; ++j)
-			{
+			for (j = 0; j < 11; ++j)
 				printf("%c ", exibicao[i][j]);
-			}
 			printf("\n");
 		}
-
+        
+        // Lê as coordenadas
+        int jog, lin, col;
 		printf("Insira o número da linha e da coluna, respectivamente: \n");
-		scanf("%d %d", &lin, &col); // Lê as coordenadas
+		scanf("%d %d", &lin, &col); 
 
 		// Verifica se há bomba
 		if (campo[lin][col]) // Se sim, explode e encerra o jogo
@@ -46,20 +43,51 @@ main (void)
 			break;
 		}
 		else
-		{
 			checa_bomba(lin, col); // Se não, checa quantas bombas existem ao redor
-		}
 
 	} while(1);
-
-
 
 	return 0;
 }
 
-int
-checa_bomba(int lin, int col)
+void
+prepara_exibicao()
 {
+    int i, j;
+    // Preenche as bordas esquerda e superior da matriz de exibição com suas coordenadas correspondentes 
+    exibicao[0][0] = ' ';
+    char buffer[1];
+    for (i = 1; i < 11; ++i)
+    {
+        sprintf(buffer, "%d", i-1); // converte o (int) i-1 para (char*) em decimal (%d) e o resultado é armazenado em buffer
+        exibicao[i][0] = buffer[0];
+    }
+    for (j = 1; j < 11; ++j)
+    {
+        sprintf(buffer, "%d", j-1);
+        exibicao[0][j] = buffer[0];
+    }
+    // Preenche o resto da matriz de exibição
+    for (i = 1; i < 11; ++i)
+		for (j = 1; j < 11; ++j)
+			exibicao[i][j] = '*';
+}
+
+void
+prepara_campo()
+{
+    int i, j;
+	// Preenche o campo aleatoriamente com 0 e 1 e preenche com '*' o que será exibido para o usuário
+	for (i = 0; i < 10; ++i)
+		for (j = 0; j < 10; ++j)
+			campo[i][j] = rand() % 2;
+}
+
+int
+checa_bomba (int lin, int col)
+{
+    lin++;
+    col++;
 	int i, j, qtd = 0;
 
 	for (i = lin - 1; i <= lin + 1; ++i)
